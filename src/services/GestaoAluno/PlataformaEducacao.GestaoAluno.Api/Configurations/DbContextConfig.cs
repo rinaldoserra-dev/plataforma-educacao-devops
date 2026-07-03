6bf452d7c307adf4b5acfe9d7a88b1fb74e68086
Felicio Melloni <mellonifjg@gmail.com>
2026-07-03 14:58:57 -0300

    Conflitos e voltas para o sql server

--- src/services/GestaoAluno/PlataformaEducacao.GestaoAluno.Api/Configurations/DbContextConfig.cs
+++ src/services/GestaoAluno/PlataformaEducacao.GestaoAluno.Api/Configurations/DbContextConfig.cs
@@ -7,7 +7,7 @@ namespace PlataformaEducacao.GestaoAluno.Api.Configurations
     {
         public static IServiceCollection AddDbContextConfig(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
         {
-            if (environment.IsDevelopment() || environment.EnvironmentName is "Docker" or "Testing")
+            if (environment.EnvironmentName == "Testing")
             {
                 services.AddDbContext<GestaoAlunoContext>(opt =>
                 {
